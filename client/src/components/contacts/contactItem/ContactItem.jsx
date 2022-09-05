a86419e86e094feda71cce8bc8dcf7b8274e7c6d
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-09-05 20:17:01 +0100

    Added respective icons

--- client/src/components/contacts/contactItem/ContactItem.jsx
+++ client/src/components/contacts/contactItem/ContactItem.jsx
@@ -1,40 +1,67 @@
 import React from "react";
+import {
+  FiEdit,
+  FiMail,
+  FiLinkedin,
+  FiTwitter,
+  FiInstagram,
+  FiPhoneCall,
+  FiUser,
+  FiUsers,
+  FiImage,
+} from "react-icons/fi";
+import { MdOutlineDelete } from "react-icons/md";
 import "./contactItem.scss";
 
 export const ContactItem = () => {
   return (
     <div className="card">
-      <div className="img">
+      <div className="card__avatar">
         <img src="" alt="avatar" />
+        <FiImage />
       </div>
       <div className="social-links">
         <a href="twitter">
-          <span className="social-link"></span>
+          <span className="social-link">
+            <FiTwitter />
+          </span>
         </a>
         <a href="instagram">
-          <span className="social-link"></span>
+          <span className="social-link">
+            <FiInstagram />
+          </span>
         </a>
         <a href="linked">
-          <span className="social-link"></span>
+          <span className="social-link">
+            <FiLinkedin />
+          </span>
         </a>
       </div>
       <div>
         <div className="input_group">
-          <i>Name: </i> <span>Emmanuel Onyema</span>
+          <i>Name: </i> <FiUser />
+          <span>Emmanuel Onyema</span>
         </div>
         <div className="input_group">
-          <i>Email: </i> <span>cemmanuelonyema@gmail.com</span>
+          <i>Email: </i>
+          <FiMail /> <span>cemmanuelonyema@gmail.com</span>
         </div>
         <div className="input_group">
-          <i>Phone: </i> <span>07025053595</span>
+          <i>Phone: </i> <FiPhoneCall />
+          <span>07025053595</span>
         </div>
         <div className="input_group">
-          <i>Label: </i> <span>Family</span>
+          <i>Label: </i> <FiUsers />
+          <span>Family</span>
         </div>
       </div>
       <div className="btns">
-        <span className="edit"></span>
-        <span className="delete"></span>
+        <div className="edit">
+          <FiEdit />
+        </div>
+        <div className="delete">
+          <MdOutlineDelete />
+        </div>
       </div>
     </div>
   );
