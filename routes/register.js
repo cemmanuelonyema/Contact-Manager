881139bd90c11b167c8960112d78eb48dcd4134d
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-09-08 12:06:22 +0100

    Add user model, validate user data from req.body

--- routes/register.js
+++ routes/register.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator/check");
 const router = express.Router();
+
+const User = require("../models/User");
 
 /*************************************************** */
 // @http function:              Sending data to the server.
@@ -7,8 +10,23 @@ const router = express.Router();
 // @desc                            Register a user
 // @access                         Public
 
-router.post("/", (req, res) => {
-  res.json({ msg: "Register a user" });
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required").notEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 8 or more characters"
+    ).isLength({ min: 8 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.json({ res: req.body });
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => res.status(200).json({ msg: "Hello world" }));
+app.get("/", (req, res) =>
+  res
+    .status(200)
+    .json({ msg: "Hello World, Welcome to thr Contact Manager Api" })
+);
+
+//Init Middleware  - provide the ability to use req.body
+app.use(express.json({ extended: false }));
 
 //Define Routes
 app.use("/api/v1/register", require("./routes/register"));
