356717a5f940a7013e06a80104066ab6df23002a
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-09-08 12:44:34 +0100

    Auth a new user and hash password

--- routes/register.js
+++ routes/register.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator/check");
 const router = express.Router();
 
@@ -20,12 +21,42 @@ router.post(
       "Please enter a password with 8 or more characters"
     ).isLength({ min: 8 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.json({ res: req.body });
+
+    //pull out  req fields
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      //if user already exists, don't register and terminate
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      //else register
+      user = new User({
+        //from the fields pulled out
+        name,
+        email,
+        password,
+      });
+
+      // and hash the password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      //then save into the database
+
+      //and return the jwtToken
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ msg: "Server Error" });
+    }
   }
 );
 
