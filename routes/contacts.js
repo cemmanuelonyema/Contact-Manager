5851d0de346a39f0e2904fb50535f21ef0b6563a
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-09-16 01:45:36 +0100

    update and delete local demo

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const { check, validationResult } = require("express-validator");
 const Contact = require("../models/Contact");
+const auth = require("../middleware/auth");
 
 /************************************************** */
 // @http function:              Getting data from the server.
@@ -10,7 +11,7 @@ const Contact = require("../models/Contact");
 // @desc                            Get all of a user's contacts
 // @access                         Private
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const contacts = await Contact.find({ user: req.user.id }).sort({
       date: -1,
@@ -31,9 +32,12 @@ router.get("/", async (req, res) => {
 router.post(
   "/",
   [
-    check("name", "Name is required").notEmpty(),
-    check("email", "Email is required").notEmpty(),
-    check("phone", "Phone Number is required").notEmpty(),
+    auth,
+    [
+      check("name", "Name is required").notEmpty(),
+      check("email", "Email is required").notEmpty(),
+      check("phone", "Phone Number is required").notEmpty(),
+    ],
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -46,13 +50,13 @@ router.post(
 
     try {
       const newContact = new Contact({
-        name,
-        email,
-        phone,
-        label,
-        instagram,
-        twitter,
-        linkedIn,
+        name: name ? name : "",
+        email: email ? email : "",
+        phone: phone ? phone : "",
+        label: label ? label : "",
+        instagram: instagram ? instagram : "",
+        twitter: twitter ? twitter : "",
+        linkedIn: linkedIn ? linkedIn : "",
         user: req.user.id,
       });
 
@@ -71,8 +75,55 @@ router.post(
 // @desc                            Update a user's contact
 // @access                         Private
 
-router.put("/:id", (req, res) => {
-  res.json({ msg: "Update  contact " });
+router.put("/:id", async (req, res) => {
+  console.log(req.body);
+  const { name, email, phone, label, instagram, twitter, linkedIn } = req.body;
+
+  const updatedContact = new Contact({
+    _id: req.params.id,
+    name: name ? name : "",
+    email: email ? email : "",
+    phone: phone ? phone : "",
+    label: label ? label : "",
+    instagram: instagram ? instagram : "",
+    twitter: twitter ? twitter : "",
+    linkedIn: linkedIn ? linkedIn : "",
+  });
+
+  try {
+    const contact = await Contact.updateOne(
+      { _id: req.params.id },
+      updatedContact
+    );
+    console.log(contact);
+
+    res.status(201).json(contact);
+  } catch (err) {
+    res.json({ err: err.response.msg });
+  }
+
+  //   if (name) userContact.name = name;
+  //   if (email) userContact.email = email;
+  //   if (phone) userContact.phone = phone;
+  //   if (label) userContact.label = label;
+  //   if (instagram) userContact.twitter = twitter;
+  //   if (twitter) userContact.instagram = instagram;
+  //   if (linkedIn) userContact.linkedIn = linkedIn;
+
+  //   try {
+  //     // get contact id to be updated
+  //     let contact = await Contact.findById(req.params.id);
+  //     if (!contact) return res.status(404).json({ msg: "Contact not found" });
+
+  //     // Make sure user owns contact
+  //     if (contact.user.toString() !== req.user.id) {
+  //       return res.status(401).json({ msg: "Not authorized" });
+  //     }
+  //   } catch (err) {
+
+  //   }
+
+  //   res.json({ msg: "Update  contact " });
 });
 
 /*************************************************** */
@@ -81,7 +132,14 @@ router.put("/:id", (req, res) => {
 // @desc                            Delete a user
 // @access                         Private
 
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "Delete a contact" });
+router.delete("/:id", async (req, res) => {
+  try {
+    await Contact.deleteOne({ _id: req.params.id });
+    res.status(200).json({ msg: "Deleted successfully" });
+  } catch (err) {
+    res.json({ err: err.response.msg });
+  }
+
+  //   res.json({ msg: "Deleted a contact successfully" });
 });
 module.exports = router;
